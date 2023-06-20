19b9167ccca4b778214c5a0d0901e26c3b689fe8
akashisthecode <137034307+akashisthecode@users.noreply.github.com>
2023-06-20 13:58:53 +0530

    Add files via upload

---
+++ Multiplication Table.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num = int(input('ENTER THE NUMBER :'))
+i = 1
+print("MULTIPLICATION TABLE: \n")
+for i in range(1,11):
+  print(num,'X',i,'=',num*i)
+
+
+# In[ ]:
+
+
+
+
